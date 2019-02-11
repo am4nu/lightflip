95c5d7eb9f0ac0791c2272120d4951d09e954474
mo <mdamanuddin@github.com>
2019-02-10 21:18:21 -0500

    OneLightVersionWorking

--- light.java
+++ light.java
@@ -24,59 +24,47 @@ public class light
 
   }
   
-  public static void lightoff(int n) //recursive light off function.
+  public static void flipswitch(int n,boolean s) //recursive flipswitch function.
   { 
    
-    if (n==1){//base case for lightoff function
+    if (n==1 && s==false){//base case for lightoff function
       arr[n]=0; 
       printsq();System.out.printf(" Turn OFF light %d\n",n);
       numofFlips++;  //counts number of flips
-     
+    
     }
-   else
-   {
-     if(n>=2){
-       lightoff(n-2); 
-       
-       arr[n]=0;
-       printsq(); System.out.printf(" Turn OFF light %d\n",n);  
-       numofFlips++;
-       lighton(n-2);
-       lightoff(n-1);
-     }
-     
-   }
-  }
-
-
-  public static void lighton(int n)
-  { 
-   
-   if (n==1){
+    if (n==1 && s==true){
       arr[n]=1;
       printsq();
       numofFlips++;
      System.out.printf(" Turn ON light %d\n",n); 
     }
    else
-   { 
-     if(n>=2){
-       lighton(n-2);
-      
-       arr[n]=1;
-       printsq();
-       numofFlips++;
-       System.out.printf(" Turn ON light %d\n",n);
-   
-       lightoff(n-2);
-       lighton(n-1);
-     }
+   {
+     if(n>=2 && s==false){
+       flipswitch(n-2,false); 
        
-  
-
- 
+       arr[n]=0;
+       printsq(); System.out.printf(" Turn OFF light %d\n",n);  
+       numofFlips++;
+       flipswitch(n-2,true); 
+       flipswitch(n-1,false);
+     }
+     if(n>=2 && s==true){
+      flipswitch(n-2,true); 
+      
+      arr[n]=1;
+      printsq(); System.out.printf(" Turn ON light %d\n",n);  
+      numofFlips++;
+      flipswitch(n-2,false); 
+      flipswitch(n-1,true);
+    }
+     
    }
   }
+
+
+ 
   public static void main(String[] args)
   {
     numofFlips=0;
@@ -84,7 +72,7 @@ public class light
 int n = sc.nextInt();
    init(n);//All lights were on initially not a big to deal to do the other way
    printsq(); System.out.printf(" %d lights are ON initially\n",n); 
-   lightoff(n);
+   flipswitch(n,false);
    System.out.println(numofFlips);
  
   }
